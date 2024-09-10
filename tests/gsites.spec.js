0c25d1810c84c203e5d190b15622d6a90fde78fb
arvin1003 <arvin@dumptruckdispatcher.com>
2024-09-10 19:38:11 +0800

    GsiteTC1

--- tests/gsites.spec.js
+++ tests/gsites.spec.js
@@ -1,26 +1,35 @@
 const { test, expect } = require('@playwright/test');
-import {url,testEmail,testPW,textContent, textSearch,antidetect,washington} from  '../testData/testData.json'
+import {url, testEmail, testPW, textContent, textSearch, antidetect, washington, gsiteTitle} from  '../testData/testData.json'
 
 test('Gsite - Test Case 1', async ({ page }) => {
     test.slow()
     //Navigate to URL
     await page.goto(url);
+    await page.waitForLoadState('domcontentloaded');
+
     //Enter Email
-    await expect(page.locator(`//div//input[@type='email']`)).toBeVisible();
+    await page.locator(`//div//input[@type='email']`).waitFor({state:'visible'});
     await page.locator(`//div//input[@type='email']`).fill(testEmail);
-    await expect(page.getByRole('button', { name: 'Next' })).toBeVisible({timeout:3000});
+    await expect(page.getByRole('button', { name: 'Next' })).toBeVisible({timeout:5000});
     await page.getByRole('button', { name: 'Next' }).click();
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(5000);
+
     //Enter Password
-    await expect(page.locator(`//div//input[@type='password']`)).toBeVisible();
+    await page.locator(`//div//input[@type='password']`).waitFor({state:'visible'});
     await page.locator(`//div//input[@type='password']`).fill(testPW);
-    await expect(page.getByRole('button', { name: 'Next' })).toBeVisible({timeout:3000});
+    await expect(page.getByRole('button', { name: 'Next' })).toBeVisible({timeout:5000});
     await page.getByRole('button', { name: 'Next' }).click();
-    await page.waitForLoadState('load');
+    await page.waitForTimeout(5000);
 
     //Click on Sites
+    await page.locator(`//img[contains(@src,'blank-googlecolors.png')]`).waitFor({state:'visible'});
     await page.locator(`//img[contains(@src,'blank-googlecolors.png')]`).click();
     await page.waitForLoadState('load');
+
+    //Rename the title
+    await expect(page.locator(`label[for='i5']`)).toBeVisible();
+    await page.locator(`label[for='i5']`).fill(gsiteTitle);
+
 
     //Populate the Blank Sheet Title
     await page.locator(`//div[@role='textbox']`).click();
@@ -41,7 +50,7 @@ test('Gsite - Test Case 1', async ({ page }) => {
     await page.keyboard.type(textSearch);
     await page.keyboard.press("Enter");
     await page.waitForLoadState('load');
-    await iframe.locator(`//div[@role='option'][1]`).click()
+    await iframe.locator(`//div[@role='option'][1]`).click();
     await iframe.getByRole('button', { name: 'Select' }).click();
 
     //Click Map
@@ -56,16 +65,26 @@ test('Gsite - Test Case 1', async ({ page }) => {
     await iframe2.locator(`//div[@class='pac-item']`).first().click();
     await iframe2.getByRole('button', { name: 'Select' }).click();
     await page.waitForTimeout(3000);
+    
     //Publish
-
     await page.locator(`//span[text()="Publish"]`).click();
     await page.waitForLoadState('load');
-    await expect(page.locator(`//input[@class='poFWNe zHQkBf']`)).toBeVisible()
-    await page.locator(`//input[@class='poFWNe zHQkBf']`).click()
-    await page.keyboard.type(antidetect);
-    await page.waitForLoadState('load');
-    await page.getByRole('button', { name: 'Publish' }).last().click();
-
+    await expect(page.locator(`//input[@class='poFWNe zHQkBf']`)).toBeVisible();
+    await page.locator(`//input[@class='poFWNe zHQkBf']`).click(); 
+    //Generate Unique Name
+    while(await page.getByRole('button', { name: 'Publish' }).last().isDisabled()){
+        const uniqueAntiDetectName = "Gsite" + Math.random().toString()
+        uniqueAntiDetectName = uniqueAntiDetectName.replace('.', "")
+        console.log(uniqueAntiDetectName)
+        await page.keyboard.type(antidetect);
+        await page.waitForLoadState('load');
+        if(await page.getByRole('button', { name: 'Publish' }).last().isEnabled())
+        {
+            await page.getByRole('button', { name: 'Publish' }).last().click();
+        }
+        await page.keyboard.press("Control+A");
+        await page.keyboard.press("Delete");
+    }
 });
 
 test.setTimeout(10000000)
