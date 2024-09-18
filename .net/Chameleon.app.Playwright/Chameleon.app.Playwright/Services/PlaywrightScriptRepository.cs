d222255035e148b39df95e838a5d14495d703b65
ezexe <elimdadia@gmail.com>
2024-09-18 12:35:37 +0300

    remove .net

--- .net/Chameleon.app.Playwright/Chameleon.app.Playwright/Services/PlaywrightScriptRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-using Chameleon.app.Playwright.Interfactes;
-
-namespace Chameleon.app.Playwright.Services;
-public class PlaywrightScriptRepository : IPlaywrightScriptRepository {
-	public IList<IBundledScript> BundledScripts { get; } = [];
-}
